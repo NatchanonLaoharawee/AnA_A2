8850b16bb1c785d6fc6f969b3933e76b2680efc2
NatchanonLaoharawee <s4006840@student.rmit.edu.au>
2024-05-30 12:05:46 +1000

    - beginning implementation of wall follower

--- mazeGenSkeleton2/solving/wallFollowingSolver.py
+++ mazeGenSkeleton2/solving/wallFollowingSolver.py
@@ -11,13 +11,25 @@ from maze.maze3D import Maze3D
 from solving.mazeSolver import MazeSolver
 from maze.util import Coordinates3D
 
+from enum import Enum
 
+class Directions(Enum):
+    """
+    Convention is (level, row, col) for constructing 3D Coordinates
+    Enums for level, row, and col
+    Has directions: North, North-East, East, South, South-West, West
+    """
+    NORTH = (0, 1, 0)
+    NORTH_EAST = (1, 0, 0)
+    EAST = (0, 0, 1)
+    SOUTH = (0, -1, 0)
+    SOUTH_WEST = (-1, 0, 0)
+    WEST = (0, 0, -1)
 
 class WallFollowingMazeSolver(MazeSolver):
     """
     Wall following solver implementation.  You'll need to complete its implementation for task B.
     """
-
 
     def __init__(self):
         super().__init__()
@@ -26,6 +38,19 @@ class WallFollowingMazeSolver(MazeSolver):
 
     def solveMaze(self, maze: Maze3D, entrance: Coordinates3D):
         # TODO: Implement this for task B!
+        self.m_solved = False
+        startCoord: Coordinates3D = entrance
+
+        # Set beginning direction
+        BEGINNING_DIRECTION = Directions.NORTH
+        
+        # Follow right wall
+        # RIGHT = 
+
+        # currCell is startCoord
+        currCell : Coordinates3D = startCoord
+        while currCell not in maze.getExits():
+            print()
         pass
 
 
